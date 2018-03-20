05d909e823cd68cc36695721b69fac5534fd3e83
Devil7 <dineshthangavel47@gmail.com>
2018-03-20 08:24:13 +0000

    AOSVP Bringup

--- aosvp.mk
+++ aosvp.mk
@@ -21,8 +21,8 @@ DEVICE_PATH := device/xiaomi/land
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSVP stuff
+$(call inherit-product, vendor/aosvp/config/common_full_phone.mk)
 
 # Inherit from land device
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -30,7 +30,7 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_land
+PRODUCT_NAME := aosvp_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -38,6 +38,9 @@ TARGET_VENDOR_PRODUCT_NAME := land
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.2.2.0.MALMIEK release-keys"
+
+# AOSVP
+AOSVP_BUILD_TYPE := OFFICIAL
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and boot image
 BUILD_FINGERPRINT := "Xiaomi/land/land:6.0.1/MMB29M/V9.2.2.0.MALMIEK:user/release-keys"
