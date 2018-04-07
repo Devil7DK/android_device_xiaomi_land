126e0215281024542ed3996cfdde44395b2debaa
nichcream <c.potato23@gmail.com>
2018-04-08 01:45:13 +0800

    land: Add libcameraservice shim to skip preview fps sanity checks
    Always support any preview frame rate by skipping preview size and
    preview frame rate checks.
    
    Change-Id: I5951cebeba860a809106749148bfc7d4798afc8f

--- device.mk
+++ device.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Libshims
+PRODUCT_PACKAGES += \
+    libshim_cameraservice
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8937
